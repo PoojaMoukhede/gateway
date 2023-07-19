d959def26bbf345bafd9567b520d6f7fb2bee6cd
Poojamoukhede <poojamoukhede27@gmail.com>
2023-07-19 12:21:48 +0530

    font size change

--- index.js
+++ index.js
@@ -18,25 +18,27 @@ function toNavigate(){
 }  
 
 // Random color generator
-
 var colors = ['#FAC5CE', '#D7F7D7', '#F5F5B8'];
 var colors2 = ['#DC3545', '#5AC55A', '#FABD20'];
 
+// function getRandomStatus() {
+//     var status = ['Offline', 'Online','Waiting'];
+//     return status[Math.floor(Math.random() * status.length)];
+// }
+
 function changeCardColor() {
-    $('.card').each(function() {
+    $('.card').each(function () {
         var randomColor = colors[Math.floor(Math.random() * colors.length)];
         var randomColor2 = colors2[colors.indexOf(randomColor)];
+        // var randomStatus = getRandomStatus();
         $(this).css('background-color', randomColor);
         $(this).find('.bookmark').css('background-color', randomColor2);
+        // $(this).find('.text-center').text(randomStatus);
     });
 }
 
-// Set an interval of 5 seconds
-setInterval(changeCardColor, 5000);
-
-// Immediately change the color after 2 seconds
-setTimeout(function() {
+// Immediately change the color and status after 2 seconds
+setTimeout(function () {
     changeCardColor();
     setInterval(changeCardColor, 5000);
 }, 2000);
-
